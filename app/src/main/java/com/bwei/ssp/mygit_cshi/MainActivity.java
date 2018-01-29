84c5b7e8d65aeda4606667bc34a8bbeaf78e96b0
songshepeng <3095842724@qq.com>
2018-01-29 14:04:40 +0800

    第一次修改

--- app/src/main/java/com/bwei/ssp/mygit_cshi/MainActivity.java
+++ app/src/main/java/com/bwei/ssp/mygit_cshi/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //测试开始
         //测试
-        //测试2
+        //测试2删除和修改的状态操作
         //工作区和缓存区
     }
 }
