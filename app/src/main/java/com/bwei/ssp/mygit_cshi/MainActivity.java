4bce7bf056967fbf3543cb9506b87bb1b860890f
songshepeng <3095842724@qq.com>
2018-01-29 11:56:22 +0800

    首次提交

---
+++ app/src/main/java/com/bwei/ssp/mygit_cshi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bwei.ssp.mygit_cshi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //测试开始
+        //测试
+    }
+}
